9742891bd43b2bca51b676addd428c737d9e4b7e
Chris Greenacre <chris.greenacre@tophhie.co.uk>
2025-11-24 20:16:15 +0000

    More branding and output

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Cloudflare Zero Trust - Blocked Request</title>
+	<meta name="theme-color" content="#100235" >
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 </svelte:head>
 
 {@render children()}
